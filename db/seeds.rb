602f60db410166924433e8ff77e4ed8d225cfd92
KATSU411 <taka.katsu411@gmail.com>
2019-10-09 10:58:55 +0900

    add seeeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times.each do|i|
+  User.create(
+    name: "test#{i}",
+    password: "aaaaaaaa"
+  )
+end
+
